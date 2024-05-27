65aa69a6011ee3f0de5a3c4b65e5600b66a20666
Michael Rudakov <mishelrudakov@yandex.ru>
2024-05-27 21:16:31 +0300

    Small refactoring + paging[WIP] d

--- app/src/main/java/com/example/esportsnews/data/mappers/NewsMappers.kt
+++ app/src/main/java/com/example/esportsnews/data/mappers/NewsMappers.kt
@@ -2,19 +2,11 @@ package com.example.esportsnews.data.mappers
 
 import com.example.esportsnews.data.local.NewsEntity
 import com.example.esportsnews.data.remote.dto.NewsResponseDto
-import com.example.esportsnews.domain.model.Article
-import com.example.esportsnews.domain.model.Source
 
 fun NewsResponseDto.toNewsEntity(): NewsEntity {
     return NewsEntity(
         articles = articles,
         status = status,
         totalResults = totalResults
-    )
-}
-
-fun NewsEntity.toArticle(): Article {
-    return Article(
-        author =
     )
 }
